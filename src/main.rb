88af76bb1ef29c81d14f06d46017ac3b1d97e03a
branyoe <bhernandez1@ucol.mx>
2023-06-09 01:50:53 -0600

    Feat: generete_table and print_table methods added

--- src/main.rb
+++ src/main.rb
@@ -4,6 +4,7 @@ require_relative 'models/vigenere_cipher'
 
 my_encrypter = VigenereCipher.new('secreto')
 
-my_encrypter.encrypt('hola')
+# my_encrypter.encrypt('hola')
 
-puts my_encrypter.adapted_secret_key
+# puts my_encrypter.adapted_secret_key
+my_encrypter.print_table
--- src/models/vigenere_cipher.rb
+++ src/models/vigenere_cipher.rb
@@ -1,12 +1,25 @@
-class VigenereCipher # rubocop:todo Style/Documentation
-  # Encrypts a message using Vigenère cipher.
-  #
-  # @param message [String] The message to be encrypted.
-  # @param key [String] The encryption key.
-  # @return [String] The encrypted message.
-  def encrypt(message, _key)
-    # If the message length is greater than the adapted_secret_key length,
-    # expand the adapted_secret_key to match the length of the message.
+
+
+class VigenereCipher
+  attr_accessor :adapted_secret_key
+  
+  def initialize(secret_key)
+    @secret_key = secret_key
+    @adapted_secret_key = secret_key
+    @alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    @table = generate_table
+  end
+
+  def print_table
+    for axisY in 0...@table.length do
+      for axisX in 0...@table.length do
+        print @table[axisY][axisX]
+      end
+      puts
+    end
+  end
+
+  def encrypt(message)
     if message.length > @adapted_secret_key.length
       loop do
         @adapted_secret_key = "#{@adapted_secret_key}#{@secret_key}"
@@ -14,18 +27,19 @@ class VigenereCipher # rubocop:todo Style/Documentation
       end
     end
 
-    # Truncate the adapted_secret_key to the length of the message.
     @adapted_secret_key = @adapted_secret_key[0...message.length]
   end
 
-  # Initializes a new instance of VigenereCipher.
-  #
-  # @param secret_key [String] The secret key for encryption.
-  def initialize(secret_key)
-    @secret_key = secret_key
-    @adapted_secret_key = secret_key
-  end
+  private
 
-  # Accessor for the adapted_secret_key attribute.
-  attr_accessor :adapted_secret_key
+  def generate_table
+    table = Array.new(27) { Array.new(27) }
+    for axisY in 0...table.length do
+      for axisX in 0...table.length do
+        alphabet_index = (@alphabet.length-axisY-axisX)*-1
+        table[axisY][axisX] = @alphabet[alphabet_index]
+      end
+    end
+    return table
+  end
 end
